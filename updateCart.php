2e379f6da544940735bf5b38a29ff937d4432d67
yashiverma0509 <101494212+yashiverma0509@users.noreply.github.com>
2022-04-21 19:57:28 +0530

    Create updateCart.php

---
+++ updateCart.php
@@ -0,0 +1,19 @@
+// Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+if (isset($_POST['update']) && isset($_SESSION['cart'])) {
+    // Loop through the post data so we can update the quantities for every product in cart
+    foreach ($_POST as $k => $v) {
+        if (strpos($k, 'quantity') !== false && is_numeric($v)) {
+            $id = str_replace('quantity-', '', $k);
+            $quantity = (int)$v;
+            // Always do checks and validation
+            if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
+                // Update new quantity
+                $_SESSION['cart'][$id] = $quantity;
+            }
+        }
+    }
+    // Prevent form resubmission...
+    header('location: index.php?page=cart');
+    exit;
+}
+
