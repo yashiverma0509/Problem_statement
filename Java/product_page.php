d028b3c1995e3c6330580ddd88bfa9e5f99b42d0
yashiverma0509 <101494212+yashiverma0509@users.noreply.github.com>
2022-04-21 19:49:09 +0530

    Create product_page.php

---
+++ Java/product_page.php
@@ -0,0 +1,15 @@
+#creating the product page
+
+<?php
+// The amounts of products to show on each page
+$num_products_on_each_page = 4;
+// The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
+$current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
+// Select products ordered by the date added
+$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT ?,?');
+// bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
+$stmt->bindValue(1, ($current_page - 1) * $num_products_on_each_page, PDO::PARAM_INT);
+$stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the products from the database and return the result as an Array
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
