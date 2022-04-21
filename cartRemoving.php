195ede69b9875ebd6f9904fa515afb197f41fb1d
yashiverma0509 <101494212+yashiverma0509@users.noreply.github.com>
2022-04-21 19:59:00 +0530

    Update cartRemoving.php

--- cartRemoving.php
+++ cartRemoving.php
@@ -1,7 +1,7 @@
-removing the product from cart
-
+<?php
 // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
 if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
     // Remove the product from the shopping cart
     unset($_SESSION['cart'][$_GET['remove']]);
 }
+exit;
